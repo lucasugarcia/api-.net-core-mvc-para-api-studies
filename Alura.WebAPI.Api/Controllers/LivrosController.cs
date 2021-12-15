bf37b1d9066e6e9a9f9a2893d6d776f35a65851e
Lucas Garcia <lucasugarcia@gmail.com>
2021-12-14 22:04:54 -0300

    Alterando a action Detalhes da controller do projeto web para consumir os dados da api

--- Alura.WebAPI.Api/Controllers/LivrosController.cs
+++ Alura.WebAPI.Api/Controllers/LivrosController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.Api.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class LivrosController : ControllerBase
