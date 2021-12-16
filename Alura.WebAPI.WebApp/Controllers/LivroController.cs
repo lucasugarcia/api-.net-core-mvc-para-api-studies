70721f5a57b063bc718f47a29948fa6a2517b9d5
Lucas Garcia <lucasugarcia@gmail.com>
2021-12-16 14:49:24 -0300

    Alterações para fazer uma inclusão (post) e uma alteração (put) de um livro

--- Alura.WebAPI.WebApp/Controllers/LivroController.cs
+++ Alura.WebAPI.WebApp/Controllers/LivroController.cs
@@ -29,13 +29,14 @@ namespace Alura.ListaLeitura.WebApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Novo(LivroUpload model)
+        public async Task<IActionResult> Novo(LivroUpload model)
         {
             if (ModelState.IsValid)
             {
-                _repo.Incluir(model.ToLivro());
+                await _api.PostLivroAsync(model);
                 return RedirectToAction("Index", "Home");
             }
+
             return View(model);
         }
 
@@ -78,21 +79,15 @@ namespace Alura.ListaLeitura.WebApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Detalhes(LivroUpload model)
+        public async Task<IActionResult> Detalhes(LivroUpload model)
         {
             if (ModelState.IsValid)
             {
-                var livro = model.ToLivro();
-                if (model.Capa == null)
-                {
-                    livro.ImagemCapa = _repo.All
-                        .Where(l => l.Id == livro.Id)
-                        .Select(l => l.ImagemCapa)
-                        .FirstOrDefault();
-                }
-                _repo.Alterar(livro);
+                await _api.PutLivroAsync(model);
+
                 return RedirectToAction("Index", "Home");
             }
+
             return View(model);
         }
 
