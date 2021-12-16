a2aaff89464ff53a888f60ca57ed76fd90cfef19
Lucas Garcia <lucasugarcia@gmail.com>
2021-12-16 15:01:15 -0300

    Remoção da relação entre o projeto web e a camada de persistÊncia, que foi delegada para a api

--- Alura.WebAPI.WebApp/Controllers/LivroController.cs
+++ Alura.WebAPI.WebApp/Controllers/LivroController.cs
@@ -1,9 +1,6 @@
-﻿using System.Linq;
-using Alura.ListaLeitura.Persistencia;
-using Alura.ListaLeitura.Modelos;
+﻿using Alura.ListaLeitura.Modelos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Alura.ListaLeitura.HttpClients;
 
@@ -12,12 +9,10 @@ namespace Alura.ListaLeitura.WebApp.Controllers
     [Authorize]
     public class LivroController : Controller
     {
-        private readonly IRepository<Livro> _repo;
         private readonly LivroApiClient _api;
 
-        public LivroController(IRepository<Livro> repository, LivroApiClient api)
+        public LivroController(LivroApiClient api)
         {
-            _repo = repository;
             _api = api;
         }
 
@@ -61,21 +56,6 @@ namespace Alura.ListaLeitura.WebApp.Controllers
             
             return View(model.ToUpload());
         }
-
-        public Livro RecuperaLivro(int id)
-        {
-            return _repo.Find(id);
-        }
-
-        public ActionResult<LivroUpload> DetalhesJson(int id)
-        {
-            var model = RecuperaLivro(id);
-
-            if (model == null)
-                return NotFound();
-
-            return model.ToModel();
-        }  
 
         [HttpPost]
         [ValidateAntiForgeryToken]
