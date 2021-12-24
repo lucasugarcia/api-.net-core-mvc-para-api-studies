5040a63eb50e18a6e1637a48692cb1aee36e8c69
Lucas Garcia <lucasugarcia@gmail.com>
2021-12-24 15:55:25 -0300

    Implementação de autenticação a partir da API especializada

--- Alura.WebAPI.Api/Controllers/ListasLeituraController.cs
+++ Alura.WebAPI.Api/Controllers/ListasLeituraController.cs
@@ -11,7 +11,7 @@ using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
 
 namespace Alura.ListaLeitura.Api.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ListasLeituraController : ControllerBase
