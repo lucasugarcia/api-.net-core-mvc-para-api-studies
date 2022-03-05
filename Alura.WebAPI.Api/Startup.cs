50a49589a5519b67b122412142a0993526145534
Lucas Garcia <lucasugarcia@gmail.com>
2022-03-05 17:44:38 -0300

    Fazendo versionamento da api com biblioteca

--- Alura.WebAPI.Api/Startup.cs
+++ Alura.WebAPI.Api/Startup.cs
@@ -57,6 +57,8 @@ namespace Alura.WebAPI.Api
                     ValidAudience = "Postman"
                 };
             });
+
+            services.AddApiVersioning();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
