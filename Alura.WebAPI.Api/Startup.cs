9b172dfb4a06427d57fd6d4f0035bea648974602
Lucas Garcia <lucasugarcia@gmail.com>
2022-03-05 17:52:04 -0300

    Removendo o versionamento da api pela rota e configurando o versionamento por query string e cabeçalho

--- Alura.WebAPI.Api/Startup.cs
+++ Alura.WebAPI.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -58,7 +59,13 @@ namespace Alura.WebAPI.Api
                 };
             });
 
-            services.AddApiVersioning();
+            services.AddApiVersioning(options =>
+            {
+                options.ApiVersionReader = ApiVersionReader.Combine(
+                    new QueryStringApiVersionReader("api-version"),
+                    new HeaderApiVersionReader("api-version")
+                );
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
